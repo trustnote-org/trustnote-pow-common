ca7d11a84ee476a44083cce9da270facef129c70
Dream <wwuwwei@126.com>
2018-08-30 09:37:33 +0800

    comment checkPOWTypeUnitsInRightRound temperarily foro now

--- validation.js
+++ validation.js
@@ -404,8 +404,8 @@ function validateParents(conn, objJoint, objValidationState, callback){
 				var max_parent_last_ball_mci = rows[0].max_parent_last_ball_mci;
 				if (max_parent_last_ball_mci > objValidationState.last_ball_mci)
 					return callback("last ball mci must not retreat, parents: "+objUnit.parent_units.join(', '));
-					checkPOWTypeUnitsInRightRound();
-				//callback();
+					//checkPOWTypeUnitsInRightRound();
+				callback();
 			}
 		);
 	}
