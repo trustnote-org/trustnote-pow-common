5886b26d7230cf1f490c540af22ab70d43ecf063
Zhou houfa <houfa.zhou@thingtrust.com>
2018-09-18 17:27:51 +0800

    change gensis unit to /2JXOmTkFL2w0HBKMyMylwwLZg+fyhYX3wKLYqE7PL8=
    change gensis unit to /2JXOmTkFL2w0HBKMyMylwwLZg+fyhYX3wKLYqE7PL8=

--- constants.js
+++ constants.js
@@ -12,7 +12,7 @@ exports.COUNT_MC_BALLS_FOR_PAID_WITNESSING = 100;
 exports.version = '1.0';
 exports.alt = '1';
 
-exports.GENESIS_UNIT = 'plWz2a9aLp+7fQ21ODAqh4FY87jf+MVnsgPhSsAr/Ls=';
+exports.GENESIS_UNIT = '/2JXOmTkFL2w0HBKMyMylwwLZg+fyhYX3wKLYqE7PL8=';
 exports.BLACKBYTES_ASSET = '9qQId3BlWRQHvVy+STWyLKFb3lUd0xfQhX6mPVEHC2c=';
 // Pow add
 exports.FOUNDATION_ADDRESS = "JNA6YWLKFQG7PFF6F32KTXBUAHRAFSET";
