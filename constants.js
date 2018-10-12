42495eb783ba0b06b6390202f969e5a81899460c
DreamWuGit <wwuwwei@126.com>
2018-10-12 12:10:08 +0800

    update genesis unit
    update genesis unit

--- constants.js
+++ constants.js
@@ -12,7 +12,7 @@ exports.COUNT_MC_BALLS_FOR_PAID_WITNESSING = 100;
 exports.version = '1.0';
 exports.alt = '1';
 
-exports.GENESIS_UNIT = '1EUA4hJGXUbRrYzuv2+aRX8odzJcXmIj7BK7IwylmSc=';
+exports.GENESIS_UNIT = 'vwfwP+AW+EXA9Eqm3qfIY1r75csEZM4kJAsA7ehcV58=';
 
 exports.BLACKBYTES_ASSET = '9qQId3BlWRQHvVy+STWyLKFb3lUd0xfQhX6mPVEHC2c=';
 // Pow add
