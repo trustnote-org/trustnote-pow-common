4c04c71b496d753b634d3bd1baf63144b2496273
liuqixing <liuqixing@gmail.com>
2018-11-29 16:09:31 +0800

    Added log in sqlite_pool.js

--- db/sqlite_pool.js
+++ db/sqlite_pool.js
@@ -81,7 +81,9 @@ module.exports = function(db_name, MAX_CONNECTIONS, bReadOnly){
 					last_arg = function(){};
 
 				var sql = arguments[0];
-				//console.log("======= query: "+sql);
+				console.log( "======= SQLite query, SQL : " + sql );
+				console.log( "======= SQLite query, ARG : ", arguments[ 1 ] );
+
 				var bSelect = !!sql.match(/^SELECT/i);
 				var count_arguments_without_callback = bHasCallback ? (arguments.length-1) : arguments.length;
 				var new_args = [];
