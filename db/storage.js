8c1d0265bdd053300e5d8dac0665133cd1325e54
VictorZhang <victor55511@163.com>
2018-12-06 15:32:55 +0800

    fix storge IF bug

--- db/storage.js
+++ db/storage.js
@@ -635,7 +635,7 @@ function getMaxMci(conn, handleResult){
 	conn.query(
 		"SELECT MAX(main_chain_index) FROM units",
 		function(rows){
-			IF(rows.length !== 1)
+			if(rows.length !== 1)
 				throw Error("getMaxMci method can not got one mci  ")
 			handleResult(rows[0].main_chain_index);
 		}
