3ce625070138c6c948e7fed0fa6ed8c35b19bbf4
VictorZhang <victor55511@163.com>
2019-01-07 15:37:43 +0800

    fix checkProofOfWork bug

--- pow/pow.js
+++ pow/pow.js
@@ -568,7 +568,7 @@ function checkProofOfWork( oConn, objInput, sHash, nNonce, pfnCallback )
 	}
 	if ( ! objInput || 'object' !== typeof objInput )
 	{
-		throw new Error( 'call checkProofOfWork with invalid objInput' + ":" + objInput + ":"+ typeof objInput);
+		throw new Error( 'call checkProofOfWork with invalid objInput');
 	}
 	if ( ! _isValidRoundIndex( objInput.roundIndex ) )
 	{
