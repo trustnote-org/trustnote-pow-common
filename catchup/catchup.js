3635122ae36a0cfb808674e456eec39306923621
VictorZhang <victor55511@163.com>
2019-01-23 19:20:35 +0800

    modi catchup

--- catchup/catchup.js
+++ catchup/catchup.js
@@ -337,7 +337,7 @@ function prepareCatchupChain( catchupRequest, callbacks )
 						if(nextMci > sLastBallMci)
 						{
 							console.log("ccatchup prepareCatchupChain pfnNext3:"+nextMci);
-							return pfnNext();
+							goDown(sLastBallMci);
 						}
 						goDown(nextMci);
 					});
